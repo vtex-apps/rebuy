ecc0057390029031b5e1d0acc188a6edd9c3b200
Lucas Bebber <lucasbbebber@gmail.com>
2018-11-30 16:11:59 -0200

    Allows updating and not passing url to withFetch

--- react/components/withFetch.js
+++ react/components/withFetch.js
@@ -5,13 +5,30 @@ const withFetch = WrappedComponent =>
   class FetchContext extends Component {
     static displayName = `FetchContext(${WrappedComponent.displayName || WrappedComponent.name})`
     static propTypes = {
-      url: string.isRequired,
-      children: func.isRequired,
+      url: string,
+      children: func,
     }
     state = { loading: true, data: null, error: null }
 
     async componentDidMount() {
+      await this.updateUrl(this.props.url)
+    }
+
+    async componentDidUpdate(prevProps) {
       const { url } = this.props
+      if (prevProps.url !== url) {
+        await this.updateUrl(url)
+      }
+    }
+
+    async updateUrl(url) {
+      if (!url) {
+        return this.setState({
+          data: [],
+          error: null,
+          loading: false,
+        })
+      }
       const response = await fetch(url)
       if (!response.ok) {
         return this.setState({ data: [], error: response.status, loading: false})
