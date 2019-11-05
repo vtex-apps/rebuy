a683838835e8764ae6c32454b609015cecbc77c4
Tiago Gimenes <tlgimenes@gmail.com>
2019-11-05 15:20:12 -0300

    rename messages

--- react/components/OrderTotal.js
+++ react/components/OrderTotal.js
@@ -8,7 +8,7 @@ import rebuy from '../rebuy.css'
 const OrderTotal = ({ value = 0 }) => (
   <div className={`${rebuy.totalContainer} flex justify-end items-end mt6-s`}>
     <span className={`${rebuy.totalTitle} t-heading-5 c-on-base`}>
-      <FormattedMessage id="rebuy.total" />
+      <FormattedMessage id="store/rebuy.total" />
     </span>
     <span className={`${rebuy.totalPrice} ml4`}>
       <ProductPrice sellingPriceClass="c-on-base t-heading-5" sellingPrice={value / 100} showListPrice={false} showLabels={false} />
